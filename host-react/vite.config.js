37fbacfec9e5e1760428c8e89957265e8a57db4f
Deki Riana <52551269+dekiriana@users.noreply.github.com>
2026-03-06 07:48:46 +0700

    Update vite.config.js

--- host-react/vite.config.js
+++ host-react/vite.config.js
@@ -1,5 +1,4 @@
-// host-react/vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 
